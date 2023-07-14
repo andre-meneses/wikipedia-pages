01b9f0a45d1e29e17bc554a12087c8bec1975ad5
André Meneses <andre_meneses2001@hotmail.com>
2023-07-14 15:30:11 -0300

    Change nodes separation in plot

--- plot_metrics.py
+++ plot_metrics.py
@@ -3,8 +3,8 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def plot_centrality(subgraph, pos, centrality_measure, filename):
-    # Calculate centrality values for all nodes in the subgraph
+def plot_centrality(subgraph, centrality_measure, filename):
+ # Calculate centrality values for all nodes in the subgraph
     centrality_values = centrality_measure(subgraph)
 
     # Sort nodes based on centrality values and select top 50 nodes
@@ -12,6 +12,12 @@ def plot_centrality(subgraph, pos, centrality_measure, filename):
 
     # Create a subgraph with top nodes and their neighbors
     subgraph_top = subgraph.subgraph(top_nodes + list(subgraph.neighbors(n) for n in top_nodes))
+
+    # Use the kamada_kawai_layout for the positions of the nodes
+    pos = nx.kamada_kawai_layout(subgraph_top)
+
+    # Adjust node spacing
+    pos = {node: (x, y*5) for node, (x, y) in pos.items()}
 
     fig, ax = plt.subplots(1, 1, figsize=(10, 8))
 
@@ -25,28 +31,26 @@ def plot_centrality(subgraph, pos, centrality_measure, filename):
     nodes = nx.draw_networkx_nodes(subgraph_top, pos=pos, node_color=color, cmap=plt.cm.jet, ax=ax)
 
     # Draw labels
-    nx.draw_networkx_labels(subgraph_top, pos=pos, font_color='white', font_size=8, ax=ax)
+    nx.draw_networkx_labels(subgraph_top, pos=pos, font_color='black', font_size=8, ax=ax)
 
     plt.axis("off")
     plt.colorbar(nodes)
     plt.savefig(filename, transparent=True, dpi=300)
     plt.show()
+    
+def plot_degree_centrality(subgraph):
+    plot_centrality(subgraph, nx.degree_centrality, 'degree_centrality.png')
+
+def plot_closeness_centrality(subgraph):
+    plot_centrality(subgraph, nx.closeness_centrality, 'closeness_centrality.png')
 
 
-def plot_degree_centrality(subgraph, pos):
-    plot_centrality(subgraph, pos, nx.degree_centrality, 'degree_centrality.png')
+def plot_betweenness_centrality(subgraph):
+    plot_centrality(subgraph, nx.betweenness_centrality, 'betweenness_centrality.png')
 
 
-def plot_closeness_centrality(subgraph, pos):
-    plot_centrality(subgraph, pos, nx.closeness_centrality, 'closeness_centrality.png')
-
-
-def plot_betweenness_centrality(subgraph, pos):
-    plot_centrality(subgraph, pos, nx.betweenness_centrality, 'betweenness_centrality.png')
-
-
-def plot_eigenvector_centrality(subgraph, pos):
-    plot_centrality(subgraph, pos, nx.eigenvector_centrality, 'eigenvector_centrality.png')
+def plot_eigenvector_centrality(subgraph):
+    plot_centrality(subgraph, nx.eigenvector_centrality, 'eigenvector_centrality.png')
 
 def plot_degree_and_centrality(subgraph):
     plt.style.use("fivethirtyeight")
